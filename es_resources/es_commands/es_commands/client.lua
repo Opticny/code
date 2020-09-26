234a923c78e01b5e6b9473c44896484434d96e62
Opticny <68088811+Opticny@users.noreply.github.com>
2020-09-26 01:20:39 -0400

    Add files via upload

---
+++ es_resources/es_commands/es_commands/client.lua
@@ -0,0 +1,22 @@
+Citizen.CreateThread(function()
+	while true do
+	Citizen.Wait(0)
+		
+	if (IsControlJustPressed(1, 207) and IsControlJustPressed(1, 317)) then
+end)
+
+RegisterNetEvent("es_commands")
+AddEventHandler("es_commands", function(t)
+	local msg = "disabled"
+	if(help == false)then
+
+        end
+        if(help)then
+                chatMessage = "/id
+                /report
+                /carry
+                /th
+                /radio
+                /emotemenu
+                /shuff"
+end)
